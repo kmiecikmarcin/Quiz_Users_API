1187c4f0beacc8fd071501c5094cba74701c9895
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2020-07-14 18:22:46 +0200

    Add first SQL steps in program

--- Routes/users.js
+++ Routes/users.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const {Client} = require('pg');
+
+var results = "";
+
+const client = new Client({
+    user: "postgres",
+    password: "axSIFux9",
+    host: "localhost",
+    port: 5432,
+    database: "Quiz_Users"
+})
 
 router.get('/loginUserInApplication', (req,res) => {
-    res.json({'jakis tekst': 2});
+    client.connect()
+    .then(() => console.log("Connected successfuly"))
+    .then(() => client.query("select * from users"))
+    .then(results => console.table(results.rows))
+    .catch(e => console.log)
+    .finally(() => client.end())
+    res.send(results);
 })
 
 module.exports = router;
