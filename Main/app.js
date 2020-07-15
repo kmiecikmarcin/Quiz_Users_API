12f71b40bc6013a42913b933cbb14697492b3bbf
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2020-07-15 16:10:38 +0200

    Add security for login to databse

--- Main/app.js
+++ Main/app.js
@@ -6,7 +6,7 @@ var app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use('/users', Routes)
 
--- Routes/users.js
+++ Routes/users.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {Client} = require('pg');
+require('dotenv/config')
 
 const client = {
-    user: "postgres",
-    password: "axSIFux9",
-    host: "localhost",
-    port: 5432,
-    database: "Quiz_Users"
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: process.env.PORT || 5432,
+    database: process.env.DATABASE_NAME
 }
 
 router.get('/loginUserInApplication', async (req,res) => {  
@@ -17,8 +18,8 @@ router.get('/loginUserInApplication', async (req,res) => {
             await db.connect()
             console.log("Connection successfully.")
     
-            const results = await db.query("Select id_user from users where user_name=($1) AND user_password=($2)", [req.body.userName,req.body.userPassword])
-            var response = (results.rows[0])
+            const results = await db.query("Select id_user,id_role from users where user_name=($1) AND user_password=($2)", [req.body.userName,req.body.userPassword])
+            var response = (results.rows)
             res.json(response) 
         }
         catch(error)
