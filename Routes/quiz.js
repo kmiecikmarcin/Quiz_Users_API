b59d7c70a4e94d2d1174df94d81100e3197f315a
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2020-08-17 15:13:19 +0200

    Added update data in question.

--- Routes/quiz.js
+++ Routes/quiz.js
@@ -8,6 +8,7 @@ const verifyToken = require('../Function/verifyJwtToken');
 const findUserByIdAndEmail = require('../Function/findUserByIdAndEmail');
 const addNewQuestion = require('../Function/addNewQuestion');
 const findTopicByName = require('../Function/findTopicByName');
+const updateQuestion = require('../Function/updateQuestion');
 const Topics = require('../Models/Topics');
 const Users = require('../Models/Users');
 const Questions = require('../Models/Questions');
@@ -69,9 +70,84 @@ router.post('/addNewQuestion',
             req.body.correctAnswer, req.body.firstAnswer, req.body.secondAnswer,
             req.body.thirdAnswer, user, topic.id);
           if (addQuestion) {
-            res.status(201).json({ Message: 'New question created!!' });
+            res.status(201).json({ Message: 'New question created!' });
             return;
           } if (addQuestion === false) {
+            res.status(400).json({ Message: 'You dont have permission!' });
+            return;
+          }
+          res.status(400).json({ Message: 'Something went wrong!' });
+        }
+      });
+    }
+  });
+
+router.put('/updateQuestion',
+  [
+    check('topicName')
+      .exists()
+      .notEmpty()
+      .isLength({ min: 1, max: 100 })
+      .not().isNumeric()
+      .trim(),
+    check('oldQuestion')
+      .exists()
+      .notEmpty()
+      .isLength({ min: 1, max: 100 })
+      .not().isNumeric()
+      .trim(),
+    check('newQuestion')
+      .exists()
+      .notEmpty()
+      .isLength({ min: 1, max: 100 })
+      .not().isNumeric()
+      .trim(),
+    check('correctAnswer')
+      .exists()
+      .notEmpty()
+      .isLength({ min: 1, max: 50 })
+      .not().isNumeric()
+      .trim(),
+    check('firstAnswer')
+      .exists()
+      .notEmpty()
+      .isLength({ min: 1, max: 50 })
+      .not().isNumeric()
+      .trim(),
+    check('secondAnswer')
+      .exists()
+      .notEmpty()
+      .isLength({ min: 1, max: 50 })
+      .not().isNumeric()
+      .trim(),
+    check('thirdAnswer')
+      .exists()
+      .notEmpty()
+      .isLength({ min: 1, max: 50 })
+      .not().isNumeric()
+      .trim(),
+  ],
+  verifyToken, (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      res.status(400).json({ Error: error });
+    } else {
+      jwt.verify(req.token, process.env.secretKey, async (err, authData) => {
+        if (err) {
+          res.sendStatus(403);
+        } else {
+          const user = await findUserByIdAndEmail(Users, authData);
+          if (user === null) { res.status(400).json({ Error: 'User doesnt exists!' }); return; }
+          const topic = await findTopicByName(Topics, req.body.topicName);
+          if (topic === null) { res.status(400).json({ Error: 'Topic doesnt exists!' }); return; }
+
+          const update = await updateQuestion(Questions, req.body.oldQuestion,
+            req.body.newQuestion, req.body.correctAnswer, req.body.firstAnswer,
+            req.body.secondAnswer, req.body.thirdAnswer, user, topic.id);
+          if (update) {
+            res.status(201).json({ Message: 'Question updated!' });
+            return;
+          } if (update === false) {
             res.status(400).json({ Message: 'You dont have permission!' });
             return;
           }
