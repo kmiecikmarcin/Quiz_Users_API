be2b6a09c44cdda1a1126cd932ad6ab21a8ed6e8
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2020-08-25 15:37:59 +0200

    Fix

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize(process.env.DATABASE_NAME,
-  process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST,
+module.exports = new Sequelize('ddf3s6s3rio39c',
+  'vamfrgtfvrgtwy', '5ae9d04520ac6dca059bef14215cfee4f94a36c87714af91c05840793d1bbbaa', {
+    host: 'ec2-54-247-103-43.eu-west-1.compute.amazonaws.com',
     dialect: 'postgres',
   });
