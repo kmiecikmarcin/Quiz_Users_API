b1e35552916bd99ec5b837dd5649d4bd4abd64aa
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2020-08-25 16:32:11 +0200

    Fix

--- config/database.js
+++ config/database.js
@@ -1,3 +1,7 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize(process.env.DATABASE_URL);
+module.exports = new Sequelize(process.env.S3_DATABASE_NAME,
+  process.env.S3_DATABASE_USER, process.env.S3_DATABASE_PASSWORD, {
+    host: process.env.S3_DATABASE_HOST,
+    dialect: 'postgres',
+  });
