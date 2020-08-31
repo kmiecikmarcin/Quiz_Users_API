f25ef586bb73ec3e102804521f3fec9f000bce05
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2020-08-31 16:37:41 +0200

    Update

--- Function/findQuestionsByTopicId.js
+++ Function/findQuestionsByTopicId.js
@@ -1,7 +1,8 @@
 async function findQuestionsByTopicId(Questions, topic) {
   const questions = await Questions.findAll({ where: { id_topic: topic } });
   if (questions.length !== 0) {
-    return questions;
+    const response = questions.map(res => res.id_topic);
+    return response;
   }
   return null;
 }
